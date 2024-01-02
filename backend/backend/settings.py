e8919cde682c0c6b92e1ef2c2441c0622608b074
CaramelInvestor <caramelinvestor@gmail.com>
2024-01-02 11:55:41 +0100

    registered the routes on the app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Custom app on the site
+    'rest-framework',
+    'posts',
+    'auth',
+    'profiles',
 ]
 
 MIDDLEWARE = [
