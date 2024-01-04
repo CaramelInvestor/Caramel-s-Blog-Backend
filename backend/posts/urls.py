407af7762f0656edadd1fa3dd9db220be3e12c1c
CaramelInvestor <caramelInvestor@gmail.com>
2024-01-04 20:29:40 +0100

    updates

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,1 +1,15 @@
- 
+# This handles the urls for the posts app
+# --------------------------------
+
+from django.urls import path
+from . import views
+
+app_name = 'post'
+
+urlpatterns = [
+    path('list/', views.post_list, name='post_list'),
+    path('detail/<slug>/', views.post_detail, name='post_detail'),
+    path('create/', views.post_create, name='post_create'),
+    path('update/<slug>/', views.post_update, name='post_update'),
+    path('delete/<slug>/', views.post_delete, name='post_delete'),
+]
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+'''This file contains the views for the posts app'''
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+
+@api_view(['GET'])
+def post_list(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def post_create(request):
+    serializer = PostSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+@api_view(['PUT'])
+def post_update(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    serializer = PostSerializer(instance=post, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
+
+@api_view(['DELETE'])
+def post_delete(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    post.delete()
+    return Response(status=204)
